467f4793e9b2fee10409eb39f6fef220de326a9a
clmonahan <chelsealeanna@gmail.com>
2019-06-01 12:31:13 -0700

    updates to app.py

--- app.py
+++ app.py
@@ -6,44 +6,21 @@ import pymysql
 from sqlalchemy import create_engine
 # from flask.ext.sqlalchemy import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
+from secrets import username, password
 
 pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@server/latte_effect'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@server/latte_effect'
 db = SQLAlchemy(app)
 
-# engine = create_engine("mysql://username:password@server/latte_effect")
-# conn = engine.connect()
+rds_connection_string = f"{username}:{password}@127.0.0.1:3306"
+engine = create_engine(f'mysql://{rds_connection_string}')
+conn = engine.connect()
 
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True)
-#     email = db.Column(db.String(120), unique=True)
-
-#     def __init__(self, username, email)
-#         self.username = username
-#         self.email = email
-
-#     def __repr__(self):
-#         return '<User %r>' % self.username
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@server/db'
-# db = SQLAlchemy(app)
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True)
-#     email = db.Column(db.String(120), unique=True)
-
-#     def __init__(self, username, email)
-#         self.username = username
-#         self.email = email
-
-#     def __repr__(self):
-#         return '<User %r>' % self.username
+# connection_string = f"{username}:{password}@127.0.0.1:3306/Exercise_Raw"
+# engine2 = create_engine(f'mysql://{connection_string}')
+# conn2 = engine2.connect()
 
 #################################################
 # Database Setup
@@ -51,18 +28,33 @@ db = SQLAlchemy(app)
 
 @app.route("/macros")
 def macrosData():
-	macrosStuff = pd.read_sql("SELECT * FROM `menu_items`", conn)
+	macrosStuff = pd.read_sql("SELECT * FROM latte_effect.menu_items ORDER BY Item", conn)
 	macrosStuff.set_index("Item",inplace = True)
 
 	macrosData = macrosStuff.to_dict(orient = "index")
 	
 	return jsonify(macrosData)
 
+@app.route("/exercise")
+def exerciseData():
+    exerciseStuff = pd.read_sql("SELECT * FROM exercise_raw.`Calories per Minute`", conn )
+    exerciseStuff.set_index("Exercise", inplace = True)
+    # print(exerciseStuff.columns)
+    exerciseData = exerciseStuff.to_dict(orient = "index")
+
+    return jsonify(exerciseData)
+    # return jsonify(exercise_data)
+
 
 @app.route("/")
 def index():
     """Return the homepage."""
     return render_template("index.html")
+
+# @app.route("/data"):
+# def get_data():
+    # logic that gets data from sql
+    # return jsonfiy(results)
 
 if __name__ == "__main__":
     app.run(debug=True)

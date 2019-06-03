8a982b4c41d54efc8df85f5a2281e60ffec7b508
Leli <zhiyinn.95@gmail.com>
2019-06-03 00:20:23 -0700

    made changes to html & added function for bar chart

--- static/app.js
+++ static/app.js
@@ -6,9 +6,8 @@
 
 // Current weight dropdown
 var weight = ["100", "125", "150", "175", "200", "250", "300"];
-  
-function init2w(array) {
-  console.log('hello')
+
+function init2w() {
   var selector = d3.select('#selDatasetWeight')
   weight.forEach(w => {
     selector
@@ -17,149 +16,169 @@ function init2w(array) {
       .property("value", w);
   });
 };
-init2w(weight);
-
 
 
 // Daily calorie dropdown
-  // Start with 1000 calories and end with 3200 by 100s
-  // Use .well and #calorie
+// Start with 1000 calories and end with 3200 by 100s
+// Use .well and #calorie
 
-  var calories = ["1000", "1250", "1500", "1750", "2000", "2500", "3000"];
+var calories = ["1000", "1250", "1500", "1750", "2000", "2500", "3000"];
+
+function init3c(array) {
+  var selector = d3.select('#selDatasetCalories')
+  calories.forEach(c => {
+    selector
+      .append("option")
+      .text(c)
+      .property("value", c);
+  });
+};
+init3c(calories);
+
+function buildBar(sample) {
   
-  function init3c(array) {
-    var selector = d3.select('#selDatasetCalories')
-    calories.forEach(c => {
-      selector
-        .append("option")
-        .text(c)
-        .property("value", c);
-    });
-  };
-  init3c(calories);
-
-
-// Calorie reader
-function buildCharts(sample) {
-
+  var goal = document.getElementById('selDatasetCalories').value
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
   var chartsURL = "/macros";
   d3.json(chartsURL).then(function (data) {
 
     var foodItem = data[sample]
-    var nutriKey = []
-    var nutriValue = []
+    var nutriKey = [];
+    var nutriValue = [];
 
+    //get calorie data 
     for (let nutriData in foodItem) {
-      if (nutriData != "Calories") {
+      if (nutriData == "Calories") {
         nutriValue.push(foodItem[nutriData])
         nutriKey.push(nutriData)
       }
     }
 
 
-    //  @TODO: Build a Pie Chart
-    var canvas = d3.select("#pie")
+    //  @TODO: Build a Bar Chart
+    var canvas = d3.select("#barDiv")
       .append("svg:svg")
       .attr("width", 300)//canvasWidth)
       .attr("height", 300);//canvasHeight);
 
+    var itemCal = parseInt(nutriValue) 
 
-    var data = [{
-      values: nutriValue.slice(0, 4),
-      labels: nutriKey.slice(0, 4),
-      type: 'pie',
-    }];
+    var trace1 = {
+      x: [itemCal],
+      y: [sample],
+      name: 'Menu Item Calories',
+      orientation: 'h',
+      marker: {
+        color: 'rgba(55,128,191,0.6)',
+        width: 1
+      },
+      type: 'bar'
+    };
 
-    // var data = [{
-    //   values: nutriValue.slice(0, 4),
-    //   labels: nutriKey.slice(0, 4),
-    //   type: 'sunburst',
-    //   outsidetextfont: {size: 20, color: "#377eb8"},
-    //   leaf: {opacity: 0.4},
-    //   marker: {line: {width: 2}}
-    // }];
+    var trace2 = {
+      x: [goal - itemCal],
+      y: [sample],
+      name: 'Calories Left Today',
+      orientation: 'h',
+      type: 'bar',
+      marker: {
+        color: 'rgba(255,153,51,0.6)',
+        width: 1
+      }
+    };
 
-    // var layout = {
-    //   margin: {l:0, r:0, b:0, t:0},
-    //   // width: 500,
-    //   // height: 500
-    //   showlegend: true,
-    // };
-
-    // Plotly.newPlot('pie', data, layout);
-
-    // console.log(data)
-    
-    
-    
-    
-    // layout = go.Layout(
-    //   legend=dict(orientation="h")
-    // )
-    // fig = go.Figure(data=data, layout=layout)
-    
-    // py.iplot(fig, filename='horizontal-legend')
+    var data = [trace1, trace2];
+    console.log(data)
 
     var layout = {
-      showlegend: true,
+      title: 'Calories for Selected Item',
+      barmode: 'stack'
     };
-    Plotly.newPlot('pie', data, layout);
 
+    Plotly.newPlot('barDiv', data, layout);
   })
 }
-// Calorie reader
-// Pulls from full menu csv
-// Use #calories-viz
 
-// Nutrition word cloud
-// Pulls from full menu csv
-// Use #words-viz section
+  // Calorie reader
+  function buildPie(sample) {
 
-// Lollipop chart
-// Pulls from full menu csv + exercise csv
-// 5 random activities pop up
-// Use #lollipop
 
-function init() {
-  // grab a reference to the dropdown select element "#item"
-  var selector = d3.select("#selDatasetItem");
+    // @TODO: Use `d3.json` to fetch the sample data for the plots
+    var chartsURL = "/macros";
+    d3.json(chartsURL).then(function (data) {
 
-  // use list of sample names to populate the select options
-  d3.json("/macros").then((foodNames) => {
-    var items = Object.keys(foodNames)
-    items.forEach((food) => {
-      // console.log(food)
-      selector
-        .append("option")
-        .text(food)
-        .property("value", food);
+      var foodItem = data[sample]
+      var nutriKey = [];
+      var nutriValue = [];
+
+      //get macros data 
+      for (let nutriData in foodItem) {
+        if (nutriData != "Calories") {
+          nutriValue.push(foodItem[nutriData])
+          nutriKey.push(nutriData)
+        }
+      }
+
+      //  @TODO: Build a Pie Chart
+      var canvas = d3.select("#pie")
+        .append("svg:svg")
+        .attr("width", 300)//canvasWidth)
+        .attr("height", 300);//canvasHeight);
+
+
+      var pieData = [{
+        values: nutriValue.slice(0, 4),
+        labels: nutriKey.slice(0, 4),
+        type: 'pie',
+      }];
+
+      var pielayout = {
+        showlegend: true,
+      };
+
+
+      Plotly.newPlot('pie', pieData, pielayout);
+
+    })
+  }
+
+  function init() {
+    // grab a reference to the dropdown select element "#item"
+    var selector = d3.select("#selDatasetItem");
+
+    // use list of sample names to populate the select options
+    d3.json("/macros").then((foodNames) => {
+      var items = Object.keys(foodNames)
+      items.forEach((food) => {
+        // console.log(food)
+        selector
+          .append("option")
+          .text(food)
+          .property("value", food);
+      });
+      const firstSample = "8-Grain Roll";
+      console.log(firstSample);
+      buildPie(firstSample);
+      buildBar(firstSample);
     });
-    const firstSample = foodNames[0];
-    console.log(foodNames[0]);
-    buildCharts(firstSample);
-  });
-};
+  };
 
-function optionChanged(newSample) {
-  console.log(newSample);
-  // fetch new data each time a new sample is selected
-  buildCharts(newSample);
-}
+
+  function optionChanged(newSample) {
+    // console.log(newSample);
+    buildPie(newSample);
+    buildBar(newSample);
+  }
+
+  //weight dropdown changed
+  function weightOptionChanged(newSample) {
+  }
+
+  function calOptionChanged(newSample) {
+    buildBar(newSample);
+  }
 
 
 init();
-
-
-// Nutrition aster plot
-  // Pulls from full menu csv
-  // Fat, carbs, fiber, protein
-  // Number of calories in center
-  // Use #aster-plot
-
-// Lollipop chart
-  // Pulls from full menu csv + exercise csv
-  // 5 random activities pop up
-  // Use #lollipop
-
+init2w();
